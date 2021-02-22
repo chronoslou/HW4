54275699dcf9384a5209dbae7e61f348d26f9b07
Lou Falla <lou3892@gmail.com>
2021-02-22 11:28:54 -0500

    adding css, js file, highscore html and read me

---
+++ Assets/js/script.js
@@ -0,0 +1,119 @@
+console.log("Hey this the JS!");
+
+var questions = [
+    {
+        title: "What is your favorite color?",
+        choices: ["red","blue","green","pink"],
+        answer: "green"
+    },
+    {
+        title: "What is your 2nd favorite color?",
+        choices: ["red","blue","green","pink"],
+        answer: "blue"
+    },
+    {
+        title: "What is your third favorite color?",
+        choices: ["red","blue","green","pink"],
+        answer: "red"
+    },
+];
+
+var timer;
+var time = 90;
+
+var startButton = document.querySelector("#start");
+
+function initiateQuiz() {
+    //hide start screen
+    document.querySelector("#start-screen").setAttribute("class","hide");
+
+    //unhide the questions
+    document.querySelector("#questions").removeAttribute("class");
+
+    //show time
+    document.querySelector("#time").textContent = time;
+
+    //Start timer
+    timer = setInterval(function() {
+        //decrease the time
+        time--;
+
+        //show the updated time
+        document.querySelector("#time").textContent = time;
+
+        //check if timer ran out
+        if (time <= 0) {
+            endQuiz();
+        }
+    },1000);
+    
+    //show the question
+    showQuestion(); 
+} 
+
+var questionIndex = 0;
+var score = 0;
+
+function handleQuestionClick(event) {
+
+    var target = event.target.textContent;
+    var answer = questions[questionIndex].answer;
+
+    //check if question is right
+    if (target === answer) {
+        //add to score
+        score++;
+    } else {
+        //If answer is wrong then taken 10 sec away from the timer
+        time = time -10;
+    }
+
+    //show the next question
+    questionIndex++;
+    showQuestion();
+
+    //check if we should end the quiz becasue it is the last question
+    if (questionIndex === questions.legnth) {
+        endQuiz();
+    } else {
+        showQuestion();
+    }
+}
+
+function showQuestion() {
+    //create the question html
+    var questionHTML = `
+        <h2 id="question-title">${questions[questionIndex].title}</h2>
+        <div id="choices" class="choices">
+            <div class="question-choice">${questions[questionIndex].choices[0]}</div>
+            <div class="question-choice">${questions[questionIndex].choices[1]}</div>
+            <div class="question-choice">${questions[questionIndex].choices[2]}</div>
+            <div class="question-choice">${questions[questionIndex].choices[3]}</div>
+        </div>
+    `;
+
+    //add the question to the page
+    document.querySelector("#questions").innerHTML = questionHTML;
+
+    //loop through each choices that now on the page then asign eventlistening ability
+    var questionChoiceArray = document.querySelectorAll(".question-choice");
+
+    console.log("questionChoiceArray: ",questionChoiceArray);
+    for (i = 0; i < questionChoiceArray.length; i++) {
+        //assign event listeners
+        questionChoiceArray[i].addEventListener("click", handleQuestionClick);
+    }
+}
+
+function endQuiz() {
+    //stop the timer
+    clearInterval(timer);
+
+    //hide the questions
+    document.querySelector("#questions").setAttribute("class","hide");
+
+    //show the end screen
+    document.querySelector("#end-screen").removeAttribute("class");
+}
+
+startButton.addEventListener("click", initiateQuiz);
---
+++ README.md
@@ -0,0 +1,30 @@
+# Homework 04 - Code Quiz
+
+## Link to site
+* [Live Link](https://chronoslou.github.io/HW3/) - ADD PROJECT LINK
+
+## Description
+
+App needs to dynamically update HTML and CSS via JS
+App needs to be responsive
+App needs to be timed
+App needs to store high scores in string and parse
+App nees to have a start button
+When presented with a question, the timer needs to start
+When a question is submitted, then you go to the next one
+if answer is wrong then time is subtracted by 10 sec
+
+
+## Usage
+
+
+
+### Password Criteria and Prompts
+
+
+
+## Preview of App 
+
+* This is how the app looks
+
+![Screenshot](./Assets/Screen-Shot.png) 
